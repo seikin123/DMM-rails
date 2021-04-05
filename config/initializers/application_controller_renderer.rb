1e80aa0d08e8ed684ceb9b267af270434d2c5de0
ヨシダタイキ <abc521fjr7@gmail.com>
2021-04-05 17:26:59 +0900

    [add]画像投稿機能

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '745504096f2de16c6351630ef684ad4c962f68cf7f262866f3afdb3fdefe125f27c1315488a80a06293fa4e358533159aca6e62d6825509fa4da00c01f3b4b29'
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_04_074948) do
+ActiveRecord::Schema.define(version: 2021_04_04_223553) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image_id"
   end
 
 end
